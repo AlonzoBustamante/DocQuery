14a8e4de7048313dbf34bbbca29c9db9ddc4e401
AlonzoBustamante <ispy600@gmail.com>
2025-06-24 12:00:29 +0800

    First commit of Next.js app

---
+++ src/app/Login/page.tsx
@@ -0,0 +1,22 @@
+// We will use this page to show the user Login form 
+// Show Login form 
+// Use useState with typed state 
+// on submit send POST request to /api/login
+// Save JWT token (in local Storage)(Other methods of saving ?)
+//Redirect to /dashboard
+
+import Form from '../components/Form'
+
+export default function Page(){
+
+    
+    return(
+        <div >
+            <Form/>
+        </div>
+
+
+
+    )
+}
+
